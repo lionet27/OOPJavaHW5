7b9cc4e4869f85abcc392e556362988f8bd49a89
«OlgaSoboleva27» <osoboleva27@gmail.com>
2023-03-27 03:14:15 +0300

    add Operation

---
+++ src/notebook/model/repository/Operation.java
@@ -0,0 +1,13 @@
+package notebook.model.repository;
+
+
+import java.util.List;
+
+/**
+ * Data Access Object (DAO) слой с методом бд
+ * @param <T> ��� ������� ��� ������/������.
+ */
+public interface Operation<T> {
+    List<T> readAll();
+    void saveAll(List<T> data);
+}
