408e0ad566e45d648207ea719ceb62bdb0515b33
«OlgaSoboleva27» <osoboleva27@gmail.com>
2023-03-27 03:14:47 +0300

    add User

--- src/notebook/model/User.java
+++ src/notebook/model/User.java
@@ -51,6 +51,6 @@ public class User {
 
     @Override
     public String toString() {
-        return String.format("Идентафикатор: %s\nИмя: %s,\nФамилия: %s,\nТелефон: %s", id, firstName, lastName, phone);
+        return String.format("ID: %s\nName: %s,\nSurname: %s,\nTelefone: %s", id, firstName, lastName, phone);
     }
 }
